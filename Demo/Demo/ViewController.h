2fb4271c933bd632021ee6ef78874eb346c9a40a
段子黄 <@qq.com>
2015-09-22 21:19:34 +0800

    1.初始化代码

---
+++ Demo/Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Demo
+//
+//  Created by 黄伟 on 15/9/22.
+//  Copyright (c) 2015年 itheima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Demo/Demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Demo
+//
+//  Created by 黄伟 on 15/9/22.
+//  Copyright (c) 2015年 itheima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
